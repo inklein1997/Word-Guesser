23a5cba7fc76694df573a0730ab92de0f915f025
Michael Klein <Michaelklein1997@gmail.com>
2021-12-26 12:57:17 -0600

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,10 @@ function start() {
     console.log("Game has started!");
     pickRandomWord();
     generateRandomWordArray(randomWord);
-    generateBlanks(randomWordArray);
-    console.log(blankString);
-    console.log(blankArray);
+//     generateBlanks(randomWordArray);
+//     console.log(blankString);
+//     console.log(blankArray);
+    startTimer();
 }
 
 function pickRandomWord() {
@@ -35,7 +36,19 @@ function generateRandomWordArray(randomWord) {
     console.log(randomWordArray);
 }
 
-function generateBlanks(randomWordArray) {
-    blankString = randomWordArray.map(letter => (guessedCorrect.indexOf(letter) >= 0 ? letter : "_")).join(' ');
-    blankArray = blankString.split('');
+// function generateBlanks(randomWordArray) {
+//     blankString = randomWordArray.map(letter => (guessedCorrect.indexOf(letter) >= 0 ? letter : "_")).join(' ');
+//     blankArray = blankString.split('');
+// }
+
+function startTimer() {
+    var timeLeft = 10
+    var timeInterval = setInterval(function() {
+        timeLeft--
+        timerEl.textContent = "TIME REMAINING: " + timeLeft;
+        if (timeLeft == 0) {
+            clearInterval(timeInterval);
+            timerEl.textContent = "YOU RAN OUT OF TIME"
+        }
+    }, 1000)
 }
